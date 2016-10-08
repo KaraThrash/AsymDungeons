27afaee944b9c8de70687e16515258e9fd322fd7
Kara Thrash <dvdonato@gmail.com>
2016-10-08 16:15:28 -0400

    s

--- Assets/_Scripts/DungeonMaster.cs
+++ Assets/_Scripts/DungeonMaster.cs
@@ -3,20 +3,77 @@ using System.Collections;
 
 public class DungeonMaster : Photon.MonoBehaviour
 {
+    public string monsterName;
     public int playerCount;
     public GameObject monsterObj;
     public bool isControllable;
+
+    
+    public GameObject cameraHolder;
+    public GameObject player;
+    public GameObject wayPoint;
+   
+
+    public float delay;
+    bool one_click = false;
+    bool timer_running;
+    public float time_for_double_click;
+
     // Use this for initialization
     void Start() {
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (isControllable == true)
         {
             if (Input.GetKeyUp(KeyCode.Space))
-            { SpawnEnemy("Monster", Vector3.zero); }
+            { SpawnEnemy(monsterName, Vector3.zero); }
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!one_click)
+                { // first click no previous clicks
+                    one_click = true;
+
+                    time_for_double_click = Time.time; // save the current time
+                                                       // do one click things;
+                }
+                else
+                {
+
+                    one_click = false; // found a double click, now reset
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.collider.tag == "Ground")
+                        {
+
+                            //wayPoint.transform.position = hit.point;
+
+                            SpawnEnemy(monsterName, hit.point);
+                        }
+
+                    }
+                    if (one_click)
+                    {
+                        // if the time now is delay seconds more than when the first click started. 
+                        if ((Time.time - time_for_double_click) > delay)
+                        {
+
+                            //basically if thats true its been too long and we want to reset so the next click is simply a single click and not a double click.
+
+                            one_click = false;
+
+                        }
+
+                    }
+                }
+            }
         }
     }
 
